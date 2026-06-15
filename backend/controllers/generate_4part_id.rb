e2a95cdc428cf7137106b6d70d115b5c4fc88da6
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2026-06-15 16:22:02 -0400

    gets the lastest NNN from the DB rather than counting page loads

--- backend/controllers/generate_4part_id.rb
+++ backend/controllers/generate_4part_id.rb
@@ -2,6 +2,26 @@ require 'time'
 
 class ArchivesSpaceService < Sinatra::Base
 
+  def self.next_available_number_for_year(year)
+    used_numbers = DB.open(true) do |db|
+      db[:accession]
+        .where(:id_0 => year)
+        .exclude(:id_1 => nil)
+        .select_map(:id_1)
+    end
+
+    numeric_values = used_numbers.each_with_object({}) do |raw_value, acc|
+      value = raw_value.to_s.strip
+      next unless value.match?(/\A\d+\z/)
+
+      acc[value.to_i] = true
+    end
+
+    candidate = 1
+    candidate += 1 while numeric_values[candidate]
+    candidate
+  end
+
   Endpoint.post('/plugins/generate_accession_identifiers/next')
     .description("Generate a new identifier based on the year and a running number")
     .params()
@@ -9,7 +29,7 @@ class ArchivesSpaceService < Sinatra::Base
     .returns([200, "{'year', 'YYYY', 'number', N}"]) \
   do
     year = Time.now.strftime('%Y')
-    number = Sequence.get("GENERATE_ACCESSION_IDENTIFIER_#{year}")
+    number = self.class.next_available_number_for_year(year)
 
     json_response(:year => year, :number => number)
   end
--- backend/plugin_init.rb
+++
@@ -1,2 +0,0 @@
-# Require that old missing Sequence class that broke things
-require_relative 'lib/sequence'
